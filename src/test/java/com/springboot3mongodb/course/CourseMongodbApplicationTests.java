ed6284208d58dd39a56cb212f4d9b9b3056254fe
caiomunhoz <caio.gmunhoz@gmail.com>
2023-12-10 19:12:41 -0300

    Project created

---
+++ src/test/java/com/springboot3mongodb/course/CourseMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot3mongodb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
