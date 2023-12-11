d27fea82a05d62e9dd3d2660a08f734336cccda3
caiomunhoz <caio.gmunhoz@gmail.com>
2023-12-11 19:19:34 -0300

    User insert, delete and update

--- src/main/java/com/springboot3mongodb/course/resources/UserResource.java
+++ src/main/java/com/springboot3mongodb/course/resources/UserResource.java
@@ -1,14 +1,20 @@
 package com.springboot3mongodb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot3mongodb.course.domain.User;
 import com.springboot3mongodb.course.dto.UserDTO;
@@ -27,10 +33,32 @@ public class UserResource {
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+		User user = service.fromDTO(userDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userDTO){
+		User user = service.fromDTO(userDTO);
+		user.setId(id);
+		service.update(user);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/springboot3mongodb/course/services/UserService.java
+++ src/main/java/com/springboot3mongodb/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot3mongodb.course.domain.User;
+import com.springboot3mongodb.course.dto.UserDTO;
 import com.springboot3mongodb.course.repositories.UserRepository;
 import com.springboot3mongodb.course.services.exceptions.ObjectNotFoundException;
 
@@ -14,13 +15,42 @@ import com.springboot3mongodb.course.services.exceptions.ObjectNotFoundException
 public class UserService {
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+
+	public User insert(User user) {
+		return repository.save(user);
+	}
+
+	public void delete(String id) {
+		if (repository.existsById(id)) {
+			repository.deleteById(id);
+		} else {
+			throw new ObjectNotFoundException(id);
+		}
+
+	}
+
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(user, newUser);
+		return repository.save(newUser);
+
+	}
+
+	public void updateData(User user, User newUser) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
