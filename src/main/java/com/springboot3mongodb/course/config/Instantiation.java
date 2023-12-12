5b83aeab762f2154ca3ba9c59f5317f86edc2d80
caiomunhoz <caio.gmunhoz@gmail.com>
2023-12-12 16:10:02 -0300

    Post entity

--- src/main/java/com/springboot3mongodb/course/config/Instantiation.java
+++ src/main/java/com/springboot3mongodb/course/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.springboot3mongodb.course.config;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.springboot3mongodb.course.domain.Post;
 import com.springboot3mongodb.course.domain.User;
+import com.springboot3mongodb.course.repositories.PostRepository;
 import com.springboot3mongodb.course.repositories.UserRepository;
 
 @Configuration
@@ -14,17 +18,24 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	UserRepository userRepo;
+	
+	@Autowired
+	PostRepository postRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
 		userRepo.deleteAll();
+		postRepo.deleteAll();
 		
 		User u1 = new User(null, "Ryan Gosling", "ryang@gmail.com");
 		User u2 = new User(null, "Ana de Armas", "ana@outlook.com");
 		User u3 = new User(null, "Harrison Ford", "hford@hotmail.com");
 		
+		Post p1 = new Post(null, "Went on a trip", "Going to Sao Paulo!", LocalDate.parse("11/12/2023", DateTimeFormatter.ofPattern("dd/MM/yyyy")), u1);
+		Post p2 = new Post(null, "Good morning", "Woke up happy today!", LocalDate.parse("12/12/2023", DateTimeFormatter.ofPattern("dd/MM/yyyy")), u1);
 		
 		userRepo.saveAll(Arrays.asList(u1, u2, u3));
+		postRepo.saveAll(Arrays.asList(p1, p2));
 	}
 
 }
---
+++ src/main/java/com/springboot3mongodb/course/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.springboot3mongodb.course.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springboot3mongodb.course.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
